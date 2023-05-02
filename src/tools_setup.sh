4c489cb05379cf02484894aa015e6251bce10e76
Antonije Petrovic <antonije.pet@gmail.com>
2023-05-02 15:12:33 +0200

    add instructions for preprocessing scripts

--- src/tools_setup.sh
+++ src/tools_setup.sh
@@ -9,6 +9,6 @@ rm xpdf-tools-linux-4.04.tar.gz
 # HunAlign
 wget ftp://ftp.mokk.bme.hu/Hunglish/src/hunalign/latest/hunalign-1.1.tgz
 tar zxvf hunalign-1.1.tgz
+rm hunalign-1.1.tgz
 cd hunalign-1.1/src/hunalign
 make
-rm hunalign-1.1.tgz
